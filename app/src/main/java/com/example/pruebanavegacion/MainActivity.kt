0099a6b60b932e3a758a362394be53f33a664a32
Alexander <alexander.arguello@hotmail.com>
2022-12-20 13:04:15 -0400

    layout y build features

--- app/src/main/java/com/example/pruebanavegacion/MainActivity.kt
+++ app/src/main/java/com/example/pruebanavegacion/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.pruebanavegacion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.pruebanavegacion.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding= DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        supportActionBar?.hide()
     }
 }
---
+++ app/src/main/java/com/example/pruebanavegacion/ui/GameOverFragment.kt
@@ -0,0 +1,26 @@
+package com.example.pruebanavegacion.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.pruebanavegacion.R
+
+
+class GameOverFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_over, container, false)
+    }
+
+}

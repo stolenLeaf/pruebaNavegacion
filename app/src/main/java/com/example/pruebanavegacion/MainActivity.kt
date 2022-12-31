79bf7f8437a1be5a5669618bd999c3dbc815aaae
Alexander <alexander.arguello@hotmail.com>
2022-12-31 15:40:07 -0400

    nav listener

--- app/src/main/java/com/example/pruebanavegacion/MainActivity.kt
+++ app/src/main/java/com/example/pruebanavegacion/MainActivity.kt
@@ -3,23 +3,38 @@ package com.example.pruebanavegacion
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.pruebanavegacion.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding= DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout=binding.drawerLayout
 //        supportActionBar?.hide()
         val navController = this.findNavController(R.id.MyNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController,drawerLayout)
+
+        navController.addOnDestinationChangedListener { nc:NavController, nd:NavDestination, args:Bundle? ->
+            if(nd.id==nc.graph.startDestinationId){
+               drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            }else{
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
+
+        NavigationUI.setupWithNavController(binding.navView,navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.MyNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController,drawerLayout)
     }
 }
